d88f12e41b61108245785f53df75d9ea5bf70463
Sri_Stark <77882371+srinijastark@users.noreply.github.com>
2022-05-12 02:51:19 -0400

    ONE

---
+++ Exp1_5/Assets/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static int PlayerScore1 = 0;
+
+	public GUISkin layout;
+
+	GameObject theBall;
+
+	// Use this for initialization
+	void Start () {
+		theBall = GameObject.FindGameObjectWithTag ("Ball");
+	}
+
+	public static void Score(string wallID) {
+		 if (wallID == "Bottomwall") {
+			PlayerScore1--;
+		}if (wallID == "DD") {
+			PlayerScore1++;
+		}if (wallID == "DDD") {
+			PlayerScore1++;
+		}if (wallID == "EN") {
+			PlayerScore1--;
+		}if (wallID == "ENE") {
+			PlayerScore1--;
+		}else if (wallID == "D") {
+			PlayerScore1++;
+		
+		}
+	}
+
+	void OnGUI() {
+		GUI.skin = layout;
+		GUI.Label (new Rect (Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+
+		if (GUI.Button (new Rect (Screen.width / 2 - 60, 35, 120, 53), "RESTART")) {
+			PlayerScore1 = 2;
+			theBall.SendMessage ("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+		}
+if (PlayerScore1 == 5) {
+			GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "YOU WIN!");
+			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		}
+		
+		 else if (PlayerScore1 == 0) {
+			GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "YOU LOOSE!");
+			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		}
+	}
+
+}
---
+++ Exp1_5/Assets/Player_controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controls : MonoBehaviour
+{
+public KeyCode moveLeft = KeyCode.A;
+public KeyCode moveRight = KeyCode.D;
+public float speed = 10.0f;
+public float boundX = 2.25f;
+private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+   void Start () {
+    rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update () {
+    var vel = rb2d.velocity;
+    if (Input.GetKey(moveLeft)) {
+        vel.x = -speed;
+    }
+    else if (Input.GetKey(moveRight)) {
+        vel.x = speed;
+    }
+    else {
+        vel.x = 0;
+    }
+    rb2d.velocity = vel;
+
+    var pos = transform.position;
+    if (pos.x > boundX) {
+        pos.x = boundX;
+    }
+    else if (pos.x < -boundX) {
+        pos.x = -boundX;
+    }
+    transform.position = pos;
+    }
+ }
+
